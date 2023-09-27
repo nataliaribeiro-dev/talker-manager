d0aa5335f757a9ced00fc161695f39209b79cbd9
Natalia Ribeiro <natilgr@hotmail.com>
2023-09-27 19:26:19 -0300

    implements User model - req 02

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    display_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    timestamps: false,
+    tableName: 'users'
+  });
+
+  return User;
+};
