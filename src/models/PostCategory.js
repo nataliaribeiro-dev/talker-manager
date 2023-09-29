234423d7399837c13020ae70676fabcd22e78170
Natalia Ribeiro <natilgr@hotmail.com>
2023-09-29 09:36:59 -0300

    implements req 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+      field: 'post_id',
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+      field: 'category_id',
+    },
+  }, {
+    timestamps: false,
+    tableName: 'posts_categories',
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+}
